fa87be5c1d2deb4b5a6c4d0efde6436378145494
wumo1016 <760478279@qq.com>
2020-11-21 12:00:27 +0800

    init

---
+++ 1.基本数据类型.ts
@@ -0,0 +1,17 @@
+
+// 冒号后面的都是类型
+
+// 简单类型
+const str: string = ''
+const num: number = 1
+const boolean: boolean = true
+
+// 联合类型
+let age: string | number = 2
+
+// 数组 对象 函数
+const arr: number[] = [1,2,3]
+
+// 元组 值必须与定义的类型一一对应
+const tuple: [string, number] = ['12', 123]
+
