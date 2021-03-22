124f030b6282d00b0be3ca2ebf3ca563cf46cef4
wumo1016 <760478279@qq.com>
2021-03-22 17:15:11 +0800

    update

--- src/13.声明文件.ts
+++ src/13.声明文件.ts
@@ -1,4 +1,6 @@
-
+// 文件都以 .d.ts 结尾
 // 使用 declare 来声明变量 只是为了编译检查
 // 使用 declare 声明的类型 可以在全局使用
 /* 如果 declare 重名 */
+
+let a: test123
--- src/declare.d.ts
+++ src/declare.d.ts
@@ -5,3 +5,5 @@ declare module '*.vue' // 引入.vue文件不报错
 declare module 'jquery' {
   const name: string
 }
+
+declare type test123 = 'string'
