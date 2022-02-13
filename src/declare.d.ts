45954404fce4f48f6883c7eaf05f743f0b365db0
wumo1016 <760478279@qq.com>
2022-02-13 16:17:00 +0800

    chore

--- src/declare.d.ts
+++ src/declare.d.ts
@@ -1,10 +1,14 @@
 // 仅仅用于编译检查 并不会实现功能
 // declare var jQuery: (selector: string) => any
 
+// 模块声明
 declare module '*.vue' // 引入.vue文件不报错
 declare module 'jquery' {
   const name: string
+  // export default $
+  export = $ // 兼容commonjs
 }
+// import $ from 'jquery' // 使用
 
 declare type test123 = 'string'
 
