c332988c39b7745bd57225f8917aaabf58460f1a
wumo1016 <760478279@qq.com>
2022-03-01 20:50:36 +0800

    chore

---
+++ src/14.装饰器.ts
@@ -0,0 +1,29 @@
+/* --------------------------------- 不带参数 --------------------------------- */
+;(function () {
+  function D1(targetClass) {
+    // targetClass 就是装饰的类
+    console.log(targetClass.name) // 类名
+    console.log(targetClass.age) // Test类
+  }
+
+  @D1
+  class Test {
+    static age: string = 'wyb'
+  }
+})
+
+/* --------------------------------- 带参数 --------------------------------- */
+;(function () {
+  function D1(params) {
+    return function (targetClass) {
+      console.log(params) // wyb
+      console.log(targetClass.name) // 类名
+      console.log(targetClass.age) // Test类
+    }
+  }
+
+  @D1('wyb')
+  class Test {
+    static age: string = 'wyb'
+  }
+})()
