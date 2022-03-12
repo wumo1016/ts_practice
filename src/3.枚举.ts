ae8ce9906b70fe55db717a8936c0e3b8eb5cdbf6
wumo1016 <760478279@qq.com>
2022-03-12 17:33:18 +0800

    chore(枚举)

--- src/3.枚举.ts
+++ src/3.枚举.ts
@@ -1,25 +1,38 @@
-// 普通枚举
-enum DIRECTION { // 大写是规范
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT,
-}
-// console.log(DIRECTION[0]); // 支持反举
+/* --------------------------------- 普通枚举 --------------------------------- */
+;(function () {
+  enum DIRECTION { // 大写是规范
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+  }
+  console.log(DIRECTION[0]) // 支持反举
+})
 
-// 异构枚举
-enum DIRECTION1 { // 如果有个数字，下面会递增推断。如果是字符串，下面的都必须手动声明
-  UP = 'test', // 字符串不支持反举
-  DOWN = 5,
-  LEFT,
-  RIGHT
-}
+/* --------------------------------- 异构 --------------------------------- */
+/* 
+- 如果有个数字，下面会递增推断
+- 如果是字符串，下面的都必须手动声明
+- 字符串不支持反举
+*/
+;(function () {
+  enum DIRECTION {
+    UP = 'test',
+    DOWN = 5,
+    LEFT,
+    RIGHT
+  }
+})
 
-// 常量枚举 
-const enum DIRECTION2 { // 编译的时候 获取的直接值(普通是这样DIRECTION.UP 而常量直接是它对应的值 0)
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT
-}
-// console.log(DIRECTION2.UP);
+/* --------------------------------- 常量枚举 --------------------------------- */
+/* 
+- 编译的时候 获取的直接值(普通是这样DIRECTION.UP 而常量直接是它对应的值 0)
+*/
+;(function () {
+  const enum DIRECTION {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+  }
+})
