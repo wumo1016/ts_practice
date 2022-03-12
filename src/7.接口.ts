0ea401ad54d9c0b07f63319954660a239e97148f
wumo1016 <760478279@qq.com>
2022-03-12 17:23:57 +0800

    chore(接口)

--- src/7.接口.ts
+++ src/7.接口.ts
@@ -2,60 +2,104 @@
 // interface 可以被类实现和继承
 // type 可以使用联合类型
 
-// 描述对象
-interface school {
-  readonly name: string // 只读
-  age: number // 后面的逗号可以不写 也可以是分号等
-  address?: string // ? 表示这个属性可选
-}
+/* --------------------------------- 描述对象 --------------------------------- */
+;(function () {
+  interface ISchool {
+    readonly name: string // 只读
+    age: number // 后面的逗号可以不写 也可以是分号等
+    address?: string // ? 表示这个属性可选
+  }
 
-const school1: school = {
-  name: '小红',
-  age: 123
-}
+  const school: ISchool = {
+    name: '小红',
+    age: 123
+  }
+})
 
-// 描述函数
-interface ICount {
-  (): number
-  speak(): number // 属性(函数)
-  count: number // 属性
-}
+/* --------------------------------- 描述函数 --------------------------------- */
+;(function () {
+  interface ICount {
+    (): number
+    speak(): number // 属性(函数)
+    count: number // 属性
+  }
 
-const fn: ICount = () => {
-  return fn.count++
-}
-fn.count = 11
-fn.speak = () => 0
+  const fn: ICount = () => {
+    return fn.count++
+  }
+  fn.count = 11
+  fn.speak = () => 0
+})
 
-// 接口合并 同名的接口会进行合并(已有不能覆盖 没有添加)
-interface IVegettables {
-  state: string
-  color: string
-}
+/* --------------------------------- 接口合并 --------------------------------- */
+/* 
+- 同名的接口会进行合并(已有不能覆盖 没有的添加)
+*/
+;(function () {
+  interface IVegettables {
+    state: string
+    color: string
+  }
 
-interface IVegettables {
-  size: number
-}
+  interface IVegettables {
+    size: number
+  }
+})
 
-// 接口可扩展
-interface ImySchool {
-  type: string
-  [key: string]: any // 可以添加多个任意属性
-}
+/* --------------------------------- 接口可扩展 --------------------------------- */
+;(function () {
+  interface ISchool {
+    type: string
+    [key: string]: any // 可以添加多个任意属性
+  }
+})
 
-// 接口可扩展 接口继承
-interface mySchool extends school {
-  type: string
-}
+/* --------------------------------- 接口继承 --------------------------------- */
+;(function () {
+  interface ISchool {
+    name: string
+    age: number
+  }
+  interface IMySchool extends ISchool {
+    type: string
+  }
 
-const mySchool1: mySchool = {
-  ...school1,
-  type: 'ceshi'
-}
+  const school: ISchool = {
+    name: '小红',
+    age: 123
+  }
+  const mySchool: IMySchool = {
+    ...school,
+    type: 'ceshi'
+  }
+})
 
-// 接口断言
-const school2: school = {
-  name: '小红',
-  age: 123,
-  lesson: '语文'
-} as school
+/* --------------------------------- 接口断言 --------------------------------- */
+/* 
+- 多的可以断言成少的
+*/
+;(function () {
+  interface ISchool {
+    name: string
+    age: number
+  }
+
+  const school: ISchool = {
+    name: '小红',
+    age: 123,
+    lesson: '语文'
+  } as ISchool
+})
+
+/* --------------------------------- 额外的类型检查 --------------------------------- */
+;(function () {
+  interface I {
+    name: string
+  }
+
+  const fn = (data: I) => {}
+  const obj = { name: 'wyb', age: 18 }
+
+  // fn({ name: 'wyb', age: 18 }) // 类型“{ name: string; age: number; }”的参数不能赋给类型“I”的参数
+  fn(obj) // 成立 因为obj不会经过额外属性检查 只要有 I 中的属性即可
+})
