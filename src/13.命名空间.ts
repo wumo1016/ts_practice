d72cc0343bfea3a5668e024734d83e7718bb17db
wumo1016 <760478279@qq.com>
2024-03-29 21:23:38 +0800

    chore: 命名空间

--- src/13.命名空间.ts
+++ src/13.命名空间.ts
@@ -1,3 +1,11 @@
+/* 模块和命名空间
+- 外部模块
+- 内部模块
+
+- 作用域
+  - 只要文件中出现 import / export 就被认为是一个作用域
+*/
+
 // 直接当成一个对象即可
 
 // 解决同一个文件的同名问题
@@ -9,3 +17,9 @@ namespace Home {
   }
 }
 const dog = new Home.Dog()
+
+// 开发全部采用 import export
+// 写声明文件的时候，如果模块是commonjs ， 或者想快速的导出一个值可以采用ts语法
+// export  = 'abc'
+// import x = require("xxxx")
+// 使用时可以采用es模块的方式导入
