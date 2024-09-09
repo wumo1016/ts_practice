01a5ba97508e553c4073f67c744d11de4bb11352
吴宜波 <760478279@qq.com>
2024-09-09 09:22:22 +0800

    chore: 内置类型

--- src/10.内置类型.ts
+++ src/10.内置类型.ts
@@ -1,4 +1,14 @@
 /* ---------------------- global objects -------------------------  */
+/* 
+ClipboardEvent<T = Element> 剪贴板事件对象
+DragEvent<T = Element> 拖拽事件对象
+ChangeEvent<T = Element> Change 事件对象
+KeyboardEvent<T = Element> 键盘事件对象
+MouseEvent<T = Element> 鼠标事件对象
+WheelEvent<T = Element> 滚轮事件对象
+AnimationEvent<T = Element> 动画事件对象
+TransitionEvent<T = Element> 过渡事件对象
+*/
 ;(function () {
   // global objects
   const n1 = new Date() // Date
